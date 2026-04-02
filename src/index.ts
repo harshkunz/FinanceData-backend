0bd6dfe661ef4a9a731db061ce146216c1fa5878
harshkunz <harshkunz0@gmial.com>
2026-04-03 00:58:17 +0530

    Created: Graceful Shutdown - server & db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import ENV  from './config/env';
 import { globalLimiter } from "./middlewares/rateLimiter";
+import { db } from "./config/db";
 
 import adminUsersRoutes from "./routes/admin.users.routes";
 import adminTransactionsRoutes from "./routes/admin.transactions.routes";
@@ -28,7 +29,26 @@ app.get("/", (req, res) => {
 
 const PORT = ENV.PORT;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`);
     console.log(`check http://localhost:${PORT}`);
 })
+
+const shutdown = async () => {
+    console.log("Shutting down...");
+
+    try {
+        await db.$disconnect();
+        server.close(() => {
+            console.log("server closed!");
+            process.exit(0);
+        });
+
+    } catch (error) {
+        console.error("Shutdown error:", error)
+        process.exit(1);
+    }
+}
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
